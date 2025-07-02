848b9567e1d2300ec82f4875bb5451e2f1524fd0
Andriy <jeki4an22@gmail.com>
2025-07-02 15:30:07 +0300

    forms, colorpicker, dropdown and other

--- drafts/src/App.js
+++ drafts/src/App.js
@@ -5,7 +5,8 @@ import ColorPicker from './components/ColorPicker';
 import TodoList from './components/TodoList';
 import initialTodos from './components/TodoList/todos.json';
 import Form from './components/Forms/Forms';
-import TodoEditor from './components/TodoEditor';
+import TodoEditor from './components/TodoEditor/todoEditor';
+import Filter from './components/Filter.js/Filter';
 
 // const colorPickerOptions = [
 //   { label: 'red', color: '#F44336' },
@@ -19,6 +20,21 @@ import TodoEditor from './components/TodoEditor';
 class App extends Component {
   state = {
     todos: initialTodos,
+    filter: '',
+  };
+
+  addTodo = text => {
+    console.log(text);
+
+    const todo = {
+      id: 'будь який згенерований айдішнік',
+      text,
+      completed: false,
+    };
+
+    this.setState(({ todos }) => ({
+      todos: [todo, ...todos],
+    }));
   };
 
   deleteTodo = todoId => {
@@ -28,41 +44,59 @@ class App extends Component {
   };
 
   toggleCompleted = todoId => {
-  //   this.setState(prevState => ({
-  //     todos: prevState.todos.map(todo => {
-  //       if (todo.id === todoId) {
-  //         console.log('Ми знайшли потрібний туду');
-  //         return {
-  //           ...todo,
-  //           completed: !todo.completed,
-  //         };
-  //       }
-  //       return todo;
-  //     }),
-  //   }));
-  // };
+    //   this.setState(prevState => ({
+    //     todos: prevState.todos.map(todo => {
+    //       if (todo.id === todoId) {
+    //         console.log('Ми знайшли потрібний туду');
+    //         return {
+    //           ...todo,
+    //           completed: !todo.completed,
+    //         };
+    //       }
+    //       return todo;
+    //     }),
+    //   }));
+    // };
 
-  this.setState(({todos}) => ({
-    todos: todos.map(todo => 
-      todo.id === todoId ? {...todo,completed: !todo.completed,} : todo,
-    ), 
-  }));
-}
-
-
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    }));
+  };
 
   formSubmitHandler = data => {
     console.log(data);
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleTodos = () => {
+    const { todos, filter } = this.state;
+    const normalizedFilter = this.state.filter.toLowerCase();
+
+    return this.state.todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  calculateCompletedTodos = () => {
+    const { todos } = this.state;
+    return todos.reduce((acc, todo) => (todo.completed ? acc + 1 : acc), 0);
+  };
 
   render() {
-    const { todos } = this.state;
-    const totalTodoCount = todos.length
-    const completedTodos = todos.reduce((acc, todo) => todo.completed ? acc +1 : acc,0)
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+    const completedTodos = this.calculateCompletedTodos();
+    const visibleTodos = this.getVisibleTodos();
+
     return (
       <>
-      <TodoEditor/>
+        <TodoEditor onSubmit={this.addTodo} />
+        <Filter value={filter} onChange={this.changeFilter} />
         {/* <Form onSubmit={this.formSubmitHandler} /> */}
 
         {/* <ColorPicker options={colorPickerOptions}/> */}
@@ -70,15 +104,16 @@ class App extends Component {
         <div>
           <p>Загальна кількість Todo: {todos.length}</p>
           <p>Загальна кількість виконаних Todo: {completedTodos}</p>
-
         </div>
 
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} onToggleCompleted={this.toggleCompleted}/>
-        <TodoEditor/>
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
       </>
     );
-  };
-};
+  }
+}
 
 export default App;
-
